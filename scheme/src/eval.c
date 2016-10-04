10fb3bb19c3a1d50a9e38004e25f85ad81dfdee5
Chloé JEHENNE <chloejehenne@free.fr>
2016-10-04 15:15:17 +0200

    And et Or

--- scheme/src/eval.c
+++ scheme/src/eval.c
@@ -18,6 +18,58 @@ object sfs_eval( object input ) {
 				input = input->cadr;
 				goto eval;
 			}
+			if(strncmp(input->this.pair.car->this.symbol,"and",3)==0)
+			{
+				
+				object et_logique=make_object(SFS_BOOLEAN);
+				
+				if(predicat(input->cadr)  == 0) 
+				{
+					printf("Faux\n");
+					et_logique->this.boolean= 0;
+					return et_logique;
+				}
+				if(predicat(input->caddr)  == 1)
+				{
+					printf( "Vrai\n");
+					et_logique->this.boolean = 1;
+					return et_logique;
+				}
+
+				else 
+				{
+					et_logique->this.boolean = 0;
+					return et_logique;
+				}	
+			}
+
+			if(strncmp(input->this.pair.car->this.symbol,"or",2)==0)
+			{
+				
+				object et_logique=make_object(SFS_BOOLEAN);
+				
+				if(predicat(input->cadr) == 1) 
+				{
+					printf("Vrai\n");
+					et_logique->this.boolean= 1;
+					return et_logique;
+				}
+				if(predicat(input->caddr)  == 0)
+				{
+					printf( "Faux\n");
+					et_logique->this.boolean = 0;
+					return et_logique;
+				}
+
+				else 
+				{
+					et_logique->this.boolean = 1;
+					return et_logique;
+				}	
+
+
+			}	
+
 			if(!strcmp(input->this.pair.car->this.symbol,"if")){ /* lecture d'un if */
 				if (input->this.pair.cdr->type==SFS_NIL){
 					WARNING_MSG("Pas de prédicat");
