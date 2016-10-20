5fcdd8f27f0ee6de457dad7ddcc195b37c58bd9b
Chloé JEHENNE <chloejehenne@free.fr>
2016-10-20 18:43:41 +0200

    Création fichiers primitives 1 min plus tard

---
+++ scheme/include/primitive.h
@@ -0,0 +1,15 @@
+#ifndef _PRIMITIVE_H_
+#define _PRIMITIVE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "number.h"
+#include "object.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PRIMITIVE_H_ */
