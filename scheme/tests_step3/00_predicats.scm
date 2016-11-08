3ed7984525f985abb6265c4d89f298e6538787d0
Mathias Brunet <mathias.brunet@orange.fr>
2016-11-08 16:14:53 +0100

    Prédicats en un seul test et symbol?

---
+++ scheme/tests_step3/00_predicats.scm
@@ -0,0 +1,15 @@
+;———
+; TEST_RETURN_CODE=PASS		
+; TEST_COMMENT="Test des predicats"
+;———
+
+(null? ())
+(null? "null?")
+(null? (()))
+(boolean? #t)
+(boolean? #f)
+(boolean? "boolean?")
+(boolean? (and #t #t))
+(boolean? (or #f #f))
+(symbol? symbol)
+
