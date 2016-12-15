e618a06ef30064450dede8df0d9f20efd8000ddd
Chloé JEHENNE <chloejehenne@free.fr>
2016-12-15 12:11:16 +0100

    Test global

--- scheme/tests_step4/100_Global_test.scm
+++ scheme/tests_step4/100_Global_test.scm
@@ -7,10 +7,6 @@
 10
 (+ 137 349)
 (- 1000 334)
-(* 5 99)
-(/ 10 5)
-(+ 21 35 12 7)
-(* 25 4 12)
 (+ (* 3 5) (- 10 6))
 (+ (* 3 (+ (* 2 4) (+ 3 5))) (+ (- 10 7) 6))
 (+ (* 3
@@ -18,3 +14,33 @@
          (+ 3 5)))
    (+ (- 10 7)
       6))
+
+(list 1 2 3)
+(car (list 1 2 3))
+(cdr (list 1 2 3))
+(and (= 4 3) (< 4 9))
+(define size 2)
+size
+(* 5 size)
+(define power2 (lambda (x) (* x x))) 
+(define sum-of-square (lambda (y z) (+ (power2 y) (power2 z))))
+(sum-of-square 2 3)
+(power2 21)
+
+((lambda (x y z) (+ x y (power2 z))) 1 2 3)
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+(a-plus-abs-b 3 -2)
+
+(define count ((lambda (total)
+(lambda (increment)
+(set! total (+ total increment)) total))
+0))
+(count 3)
+
+(if (= n 0)
+1
+(* n (factorial (- n 1))))))
+
+(factorial 4)
